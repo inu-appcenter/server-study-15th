02718e92c1ccc57e5d6eae668137afc172c30bc3
wellbeing-dough <kdw990202@naver.com>
2023-05-12 00:29:56 +0900

    [Fix]: 불필요한 import삭제

--- contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/common/config/SwaggerConfig.java
+++ contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/common/config/SwaggerConfig.java
@@ -2,11 +2,8 @@ package com.example.todo.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
