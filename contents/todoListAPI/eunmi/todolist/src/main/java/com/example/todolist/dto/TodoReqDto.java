4217f4b1d816c4e1102c1fba971ce5dfd984d400
jum0624 <jum0624@naver.com>
2023-05-12 04:28:42 +0900

    feat: TodoService 구현

---
+++ contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/dto/TodoReqDto.java
@@ -0,0 +1,36 @@
+package com.example.todolist.dto;
+
+import com.example.todolist.domain.Member;
+import com.example.todolist.domain.Todo;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class TodoReqDto {
+
+    private String content;
+
+    private LocalDateTime creatAt = LocalDateTime.now();
+
+    private boolean checked;
+
+    @Builder
+    public TodoReqDto(String content, LocalDateTime creatAt, boolean checked) {
+        this.content = content;
+        this.creatAt = creatAt;
+        this.checked = checked;
+    }
+
+    public Todo toEntity(Member member, TodoReqDto todoReqDto) {
+        return Todo.builder()
+                .member(member)
+                .content(todoReqDto.getContent())
+                .creatAt(todoReqDto.getCreatAt())
+                .checked(false)
+                .build();
+    }
+
+
+}
