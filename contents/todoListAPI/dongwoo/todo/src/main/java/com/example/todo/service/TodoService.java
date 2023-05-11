8a29b41b7b46045408c876e0e439b07330572613
wellbeing-dough <kdw990202@naver.com>
2023-05-12 00:31:09 +0900

    [Fix] : 불필요한 import 삭제

--- contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/service/TodoService.java
+++ contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/service/TodoService.java
@@ -13,8 +13,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.validation.Valid;
-
 @Service
 @RequiredArgsConstructor
 @Transactional
