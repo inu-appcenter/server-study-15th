7c3a145aaf11c61d9010464c51dbd45d635355c5
wellbeing-dough <kdw990202@naver.com>
2023-05-12 00:32:22 +0900

    [Fix] : 불필요한 import 삭제

--- contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/repository/TodoRepository.java
+++ contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/repository/TodoRepository.java
@@ -1,7 +1,6 @@
 package com.example.todo.repository;
 
 import com.example.todo.domain.Todo;
-import com.example.todo.dto.data.TodoListData;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
