10cb0755f4f1244cf42eb6601179143101800034
elyudwo <dudqk9696@naver.com>
2023-05-11 18:04:02 +0900

    [Refactor] 3주차 - 컨트롤러 반환값 모듈화

--- contents/todoListAPI/yeongjae/todolist/todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ contents/todoListAPI/yeongjae/todolist/todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,44 +1,51 @@
 package com.example.todolist.controller;
 
-import com.example.todolist.dto.TaskDto;
+import com.example.todolist.dto.ResponseDto;
+import com.example.todolist.dto.taskdto.TaskRequestDto;
 import com.example.todolist.service.TodoService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import static com.example.todolist.dto.taskdto.TaskResponseDto.*;
+
+@RestController
 @RequiredArgsConstructor
-@Tag(name = "Todo", description = "Todo 관련 api")
 public class TodoController {
 
     private final TodoService todoService;
 
     @GetMapping("/Todo")
-    @Operation(summary = "Todo 리스트", description = "Todo 리스트 반환 api 입니다")
+    @Operation(summary = "Todo 조회", description = "Todo 리스트를 조회합니다")
+    @ApiResponse(responseCode = "200", description = "Todo 조회 성공")
     public ResponseEntity<?> findTodo() {
-        return new ResponseEntity<>(todoService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDto<>(1,"Todo 조회 성공",todoService.findAll()), HttpStatus.OK);
     }
 
     @PostMapping("/Todo")
     @Operation(summary = "Todo 작성", description = "Todo 작성 api 입니다")
-    public ResponseEntity<?> writeTodo(@RequestBody TaskDto taskDto) {
-        todoService.writeTodo(taskDto);
-        return new ResponseEntity<>(taskDto, HttpStatus.CREATED);
+    @ApiResponse(responseCode = "201", description = "Todo 작성 성공")
+    public ResponseEntity<?> writeTodo(@RequestBody TaskRequestDto.TaskSaveReqDto TaskSaveReqDto) {
+        TaskSaveRespDto taskSaveRespDto = todoService.writeTodo(TaskSaveReqDto);
+        return new ResponseEntity<>(new ResponseDto<>(1, "Todo 작성 성공", taskSaveRespDto), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/Todo/{id}")
+    @Operation(summary = "Todo 삭제", description = "Todo 삭제 api 입니다")
+    @ApiResponse(responseCode = "200", description = "Todo 삭제 성공")
     public ResponseEntity<?> deleteTodo(@PathVariable Long id) {
-        todoService.deleteTodo(id);
-        return new ResponseEntity<>(id,HttpStatus.OK);
+        TaskDeleteRespDto taskDeleteRespDto = todoService.deleteTodo(id);
+        return new ResponseEntity<>(new ResponseDto<>(1, "Todo 삭제 성공", taskDeleteRespDto), HttpStatus.OK);
     }
 
     @PutMapping("/Todo/{id}")
-    public ResponseEntity<?> editTodo(@PathVariable Long id, @RequestBody TaskDto taskDto) throws Exception {
-        todoService.editTodo(id, taskDto);
-        return new ResponseEntity<>(taskDto, HttpStatus.OK);
+    @Operation(summary = "Todo 수정", description = "Todo 수정 api 입니다")
+    @ApiResponse(responseCode = "200", description = "Todo 수정 성공")
+    public ResponseEntity<?> editTodo(@PathVariable Long id, @RequestBody TaskRequestDto.TaskEditRequestDto taskEditRequestDto) throws Exception {
+        TaskEditRespDto taskEditRespDto = todoService.editTodo(id, taskEditRequestDto);
+        return new ResponseEntity<>(new ResponseDto<>(1, "Todo 수정 성공", taskEditRespDto), HttpStatus.CREATED);
     }
 }
---
+++ contents/todoListAPI/yeongjae/todolist/todolist/src/main/java/com/example/todolist/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.todolist.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ResponseDto<T> {
+    private final Integer code;
+    private final String msg;
+    private final T data;
+}
