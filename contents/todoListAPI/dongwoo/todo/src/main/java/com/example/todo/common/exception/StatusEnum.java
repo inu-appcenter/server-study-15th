035eb7eb0e7fe355a42ffec8d3a6fdf0bff6a77e
wellbeing-dough <kdw990202@naver.com>
2023-05-13 11:40:05 +0900

    [Fix] : 예외처리 상태코드 수정
    403 -> 409

--- contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/common/exception/StatusEnum.java
+++ contents/todoListAPI/dongwoo/todo/src/main/java/com/example/todo/common/exception/StatusEnum.java
@@ -3,7 +3,7 @@ package com.example.todo.common.exception;
 public enum StatusEnum {
     BAD_REQUEST(400, "BAD_REQUEST"),
     USER_NOT_FOUND(404,"USER_NOT_FOUND"),
-    ALREADY_USER_EXIST(403, "ALREADY_USER_EXIST"),
+    ALREADY_USER_EXIST(409, "ALREADY_USER_EXIST"),
     TODO_NOT_FOUND(404,"USER_NOT_FOUND");
 
     private final int statusCode;
