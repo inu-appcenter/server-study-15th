c2d772961d9793dacb2b6a44f01eec92621740fd
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-05-18 15:46:25 +0900

    [Fix] 불필요하게 선언된 Setter 어노테이션 및 생성자 삭제
    #30

--- contents/todoListAPI/wonjeong/src/main/java/com/appcenter/data/dto/MemberDTO.java
+++ contents/todoListAPI/wonjeong/src/main/java/com/appcenter/data/dto/MemberDTO.java
@@ -1,19 +1,14 @@
 package com.appcenter.data.dto;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
 public class MemberDTO {
 
     private String name;
     private String password;
     private String email;
 
-    public MemberDTO(String name, String password, String email) {
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
 }
