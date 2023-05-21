4d9c9a38af66b3b0808c65a8cc2090b79cd6e25c
rnwnsgud <rnwnsgud90@naver.com>
2023-05-21 19:23:34 +0900

    5주차 내용추가 및 리팩토링 #36

---
+++ contents/todoListAPI/junhyung/src/main/java/appCenter/guCoding/todoList/config/auth/LoginUser.java
@@ -0,0 +1,55 @@
+package appCenter.guCoding.todoList.config.auth;
+
+import appCenter.guCoding.todoList.domain.user.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Getter
+public class LoginUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(() -> "ROLE_" + user.getRole());
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
