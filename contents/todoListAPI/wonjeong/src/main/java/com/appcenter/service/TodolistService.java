e98af792dee97ef306a4822abc3d31aaa8eeb735
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-05-21 17:38:01 +0900

    Revert "Revert "Feat, Refactor: Member, Todolist Update, Delete 메소드 완성 및 Req, Res DTO 수정"" #38
    This reverts commit 4011649aa7f617b2d3d8de52ca26aaccf968e09a.

--- contents/todoListAPI/wonjeong/src/main/java/com/appcenter/service/TodolistService.java
+++ contents/todoListAPI/wonjeong/src/main/java/com/appcenter/service/TodolistService.java
@@ -1,15 +1,15 @@
 package com.appcenter.service;
 
-import com.appcenter.data.dto.TodolistDTO;
+import com.appcenter.data.dto.request.TodolistRequestDTO;
 import com.appcenter.data.dto.response.TodolistResponseDTO;
 
 public interface TodolistService {
 
     TodolistResponseDTO getContent(Long id) throws Exception;
 
-    TodolistResponseDTO savedContent(TodolistDTO todolistDTO);
+    TodolistResponseDTO savedContent(TodolistRequestDTO todolistRequestDTO);
 
-    TodolistResponseDTO updateContent(Long id, String name) throws Exception;
+    TodolistResponseDTO updateContent(Long id, TodolistRequestDTO todolistRequestDTO) throws Exception;
 
     void deleteContent(Long id) throws Exception;
 }
