4ed443a6f83b961f1fe9c2c6bc18d6febeba2aac
jum0624 <jum0624@naver.com>
2023-06-01 03:02:58 +0900

    refactor: Todo 응답 메세지 Message 객체로 변경

--- contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,6 +1,7 @@
 package com.example.todolist.controller;
 
 
+import com.example.todolist.domain.Message;
 import com.example.todolist.dto.TodoCheckReqDto;
 import com.example.todolist.dto.TodoPageRespDto;
 import com.example.todolist.dto.TodoReqDto;
@@ -19,32 +20,37 @@ import java.util.List;
 public class TodoController {
     private final TodoService todoService;
 
+    private static final String SUCCESS_TODO_CREATE_MESSAGE = "todo가 저장 되었습니다.";
+    private static final String SUCCESS_TODO_UPDATE_MESSAGE = "todo가 수정되었습니다.";
+    private static final String SUCCESS_TODO_CHECKED_UPDATE_MESSAGE = "수행여부가 수정되었습니다.";
+    private static final String SUCCESS_TODO_DELETE_MESSAGE = "todo가 삭제되었습니다.";
+
     @PostMapping()
-    public ResponseEntity<String> create(Long memberId, @RequestBody TodoReqDto todoReqDto) {
+    public ResponseEntity<Message> create(Long memberId, @RequestBody TodoReqDto todoReqDto) {
         Long save = todoService.save(memberId, todoReqDto);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(save + "번 할 일 등록 완료");
+                .body(new Message(SUCCESS_TODO_CREATE_MESSAGE));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<String> updateChecked(@PathVariable Long id, @RequestBody TodoCheckReqDto todoCheckReqDto) {
+    public ResponseEntity<Message> updateChecked(@PathVariable Long id, @RequestBody TodoCheckReqDto todoCheckReqDto) {
         Long updateChecked = todoService.updateChecked(id, todoCheckReqDto);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(updateChecked + "번 Checked 수정 완료");
+                .body(new Message(SUCCESS_TODO_CHECKED_UPDATE_MESSAGE));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateTodo(@PathVariable Long id, @RequestBody TodoReqDto todoReqDto) {
+    public ResponseEntity<Message> updateTodo(@PathVariable Long id, @RequestBody TodoReqDto todoReqDto) {
         Long update = todoService.update(id, todoReqDto);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(update + "번 할 일 수정 완료");
+                .body(new Message(SUCCESS_TODO_UPDATE_MESSAGE));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteTodo(@PathVariable Long id) {
+    public ResponseEntity<Message> deleteTodo(@PathVariable Long id) {
         todoService.deleteTodo(id);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(id + "번 할 일 삭제 완료");
+                .body(new Message(SUCCESS_TODO_DELETE_MESSAGE));
     }
 
     @GetMapping()
