a69c033b0390aaa189150320f97fd1ec4e7610a7
jum0624 <jum0624@naver.com>
2023-06-01 03:37:58 +0900

    add: apiOperation 추가

--- contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/controller/MemberController.java
+++ contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.example.todolist.domain.Message;
 import com.example.todolist.dto.MemberReqDto;
 import com.example.todolist.dto.MemberPageRespDto;
 import com.example.todolist.service.MemberService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,16 +20,26 @@ import java.util.List;
 public class MemberController {
 
     private final MemberService memberService;
+    private static final String SUCCESS_MEMBER_LOGIN_MESSAGE = "로그인 성공";
     private static final String SUCCESS_MEMBER_UPDATE_MESSAGE = "회원정보가 수정되었습니다.";
     private static final String SUCCESS_MEMBER_DELETE_MESSAGE = "회원정보가 삭제되었습니다.";
 
-    @PostMapping()
+    @ApiOperation(value = "로그인")
+    @PostMapping("/login")
+    public ResponseEntity<Message> login() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new Message(SUCCESS_MEMBER_LOGIN_MESSAGE));
+    }
+
+    @ApiOperation(value = "유저 등록")
+    @PostMapping("/join")
     public ResponseEntity<Message> join(@Valid @RequestBody MemberReqDto memberReqDto) {
         Message message = memberService.save(memberReqDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(message);
     }
 
+    @ApiOperation(value = "유저정보 수정")
     @PutMapping("/{id}")
     public ResponseEntity<Message> updateMember(@PathVariable Long id, @Valid @RequestBody MemberReqDto memberReqDto) {
         Long update = memberService.update(id, memberReqDto);
@@ -36,6 +47,7 @@ public class MemberController {
                 .body(new Message(SUCCESS_MEMBER_UPDATE_MESSAGE));
     }
 
+    @ApiOperation(value = "유저정보 삭제")
     @DeleteMapping("/{id}")
     public ResponseEntity<Message> deleteMember(@PathVariable Long id) {
         memberService.deleteMember(id);
@@ -43,6 +55,7 @@ public class MemberController {
                 .body(new Message(SUCCESS_MEMBER_DELETE_MESSAGE));
     }
 
+    @ApiOperation(value = "유저 조회")
     @GetMapping()
     public ResponseEntity<List<MemberPageRespDto>> readAllMember() {
         List<MemberPageRespDto> todos = memberService.findAll();
@@ -50,6 +63,7 @@ public class MemberController {
                 .body(todos);
     }
 
+    @ApiOperation(value = "유저 단일 조회")
     @GetMapping("/{id}")
     public ResponseEntity<MemberReqDto> readOneMember(@PathVariable Long id) {
         MemberReqDto member = memberService.findOne(id);
--- contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/controller/TodoController.java
+++ contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/controller/TodoController.java
@@ -6,6 +6,7 @@ import com.example.todolist.dto.TodoCheckReqDto;
 import com.example.todolist.dto.TodoPageRespDto;
 import com.example.todolist.dto.TodoReqDto;
 import com.example.todolist.service.TodoService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,8 @@ public class TodoController {
     private static final String SUCCESS_TODO_CHECKED_UPDATE_MESSAGE = "수행여부가 수정되었습니다.";
     private static final String SUCCESS_TODO_DELETE_MESSAGE = "todo가 삭제되었습니다.";
 
+
+    @ApiOperation(value = "할 일 등록", notes = "멤버id, 등록할 할일 정보 입력")
     @PostMapping()
     public ResponseEntity<Message> create(Long memberId, @RequestBody TodoReqDto todoReqDto) {
         Long save = todoService.save(memberId, todoReqDto);
@@ -30,6 +33,7 @@ public class TodoController {
                 .body(new Message(SUCCESS_TODO_CREATE_MESSAGE));
     }
 
+    @ApiOperation(value = "완료 여부 체크 수정", notes = "해당 todo id값과 체크 여부 json으로 입력")
     @PatchMapping("/{id}")
     public ResponseEntity<Message> updateChecked(@PathVariable Long id, @RequestBody TodoCheckReqDto todoCheckReqDto) {
         Long updateChecked = todoService.updateChecked(id, todoCheckReqDto);
@@ -37,6 +41,7 @@ public class TodoController {
                 .body(new Message(SUCCESS_TODO_CHECKED_UPDATE_MESSAGE));
     }
 
+    @ApiOperation(value = "할 일 수정", notes = "수정할 todo id, 수정할 할 일 정보 입력")
     @PutMapping("/{id}")
     public ResponseEntity<Message> updateTodo(@PathVariable Long id, @RequestBody TodoReqDto todoReqDto) {
         Long update = todoService.update(id, todoReqDto);
@@ -44,6 +49,7 @@ public class TodoController {
                 .body(new Message(SUCCESS_TODO_UPDATE_MESSAGE));
     }
 
+    @ApiOperation(value = "할 일 삭제", notes = "삭제할 todo id 파라미터 필요")
     @DeleteMapping("/{id}")
     public ResponseEntity<Message> deleteTodo(@PathVariable Long id) {
         todoService.deleteTodo(id);
@@ -51,6 +57,7 @@ public class TodoController {
                 .body(new Message(SUCCESS_TODO_DELETE_MESSAGE));
     }
 
+    @ApiOperation(value = "할 일 단일 조회", notes = "조회할 todo id 파라미터 필요")
     @GetMapping("/{id}")
     public ResponseEntity<TodoPageRespDto> readOneTodo(@PathVariable Long id) {
         TodoPageRespDto todo = todoService.findOneTodo(id);
@@ -58,6 +65,7 @@ public class TodoController {
                 .body(todo);
     }
 
+    @ApiOperation(value = "할 일 조회", notes = "멤버id, 조회하고 싶은 할일을 페이징으로 조회")
     @GetMapping()
     public ResponseEntity<Page<TodoPageRespDto>> readTodos(Long memberId,
                                                            @RequestParam("page") Integer page,
