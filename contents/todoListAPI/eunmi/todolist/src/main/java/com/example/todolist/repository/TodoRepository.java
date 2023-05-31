a1e3c638d27e139007cd92a8435ade0fe06f17f0
jum0624 <jum0624@naver.com>
2023-06-01 02:38:10 +0900

    refactor: todo 조회 기능 페이징 처리

--- contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/repository/TodoRepository.java
+++ contents/todoListAPI/eunmi/todolist/src/main/java/com/example/todolist/repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.example.todolist.repository;
 
 import com.example.todolist.domain.Member;
 import com.example.todolist.domain.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
