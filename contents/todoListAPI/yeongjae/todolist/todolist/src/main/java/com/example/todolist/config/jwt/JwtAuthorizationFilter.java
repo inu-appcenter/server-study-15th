4978a6beaab7fdde271b7ade765800b622eb2255
elyudwo <dudqk9696@naver.com>
2023-06-09 20:23:55 +0900

    Feat : 인가 기능 구현

---
+++ contents/todoListAPI/yeongjae/todolist/todolist/src/main/java/com/example/todolist/config/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,72 @@
+package com.example.todolist.config.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.todolist.config.auth.PrincipalDetails;
+import com.example.todolist.domain.user.User;
+import com.example.todolist.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 시큐리티가 filter 가지고있는데 그 필터중에 BasicAuthenticationFilter 라는 것이 있다.
+// 권한이나 인증이 필요한 특정 주소를 요청했을때 위 필터를 무조건 타게 되어있음
+// 만약 권한이나 인증이 필요한 주소가 아니라면 이 필터를 안탄다.
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final UserRepository userRepository;
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserRepository userRepository) {
+        super(authenticationManager);
+        this.userRepository = userRepository;
+    }
+
+
+    // 인증이나 권한이 필요한 요청이 있을 때 해당 필터를 타게된다.
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        super.doFilterInternal(request, response, chain);
+        System.out.println("인증이나 권한이 필요한 주소 요청이 됨");
+
+        String jwtHeader = request.getHeader("Authorization");
+        if (jwtHeader == null || !jwtHeader.startsWith("Bearer")) {
+            System.out.println("넘어가겠습니다");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        System.out.println("안넘어왔습니다");
+        System.out.println("jwtHeader : " + jwtHeader);
+
+        String jwtToken = request.getHeader("Authorization").replace("Bearer ", "");
+
+        String username =
+                JWT.require(Algorithm.HMAC512("cos")).build().verify(jwtToken).getClaim("username").asString();
+
+
+
+        if(username != null) {
+            User userEntity = userRepository.findByName(username);
+
+            PrincipalDetails principalDetails = new PrincipalDetails(userEntity);
+
+            // Jwt 토큰 서명을 통해서 서명이 정상이면 Authentication 객체를 만들어준다.
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(principalDetails, null, principalDetails.getAuthorities());
+
+            //강제로 시큐리티의 세션에 접근하여 Authentication 저장
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }
+        chain.doFilter(request, response);
+    }
+}
